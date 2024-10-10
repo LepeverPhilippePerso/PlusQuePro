6e578dbd898b3fe70033b364aca155e2652cd743
LepeverPhilippe <83241919+LepeverPhilippe@users.noreply.github.com>
2024-10-10 11:58:52 +0200

    React configuration

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import { Head } from '@inertiajs/react'
+
+export default function Welcome() {
+  return (
+    <>
+      <Head title="Welcome" />
+      <h1>Welcome</h1>
+      <p>Hello welcome to your first  <span style={{color:'green'}}>react-Inertia</span> and <span style={{color:'red'}}>jetstream laravel</span> app!</p>
+
+    </>
+  )
+}
