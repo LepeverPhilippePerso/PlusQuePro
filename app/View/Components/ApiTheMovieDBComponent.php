c390dbf10684a0ac5a1639605c3580a35998925b
LepeverPhilippe <83241919+LepeverPhilippe@users.noreply.github.com>
2024-10-10 17:46:13 +0200

    Add ApiTheMovieDBComponent file

---
+++ app/View/Components/ApiTheMovieDBComponent.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class ApiTheMovieDBComponent extends Component
+{
+    private $apiKey = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjJkNjNjZGRjMDY2ZDk5ZWQzZTgwNmQzMjY3MThjYSIsInN1YiI6IjYyNGVhNTRhYjc2Y2JiMDA2ODIzODc4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zuuBq1c63XpADl8SQ_c62hezeus7VibE1w5Da5UdYyo";
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.api-the-movie-d-b-component');
+    }
+}
